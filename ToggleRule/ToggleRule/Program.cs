e427771e77535f802259ab5246c9b195f518f095
DG Hades <61195031+DGHades@users.noreply.github.com>
2023-05-05 20:08:48 +0200

    Add base for C# Based Rule creator

---
+++ ToggleRule/ToggleRule/Program.cs
@@ -0,0 +1,21 @@
+﻿using NetFwTypeLib;
+
+namespace ToggleRule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+            firewallRule.Description = "Blocks connection to LoL Social Server.";
+            firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+            firewallRule.Enabled = true;
+            firewallRule.Protocol = 6;
+            firewallRule.RemotePorts = "5223"; 
+            firewallRule.Name = "LoL_Invisible";
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            firewallPolicy.Rules.Add(firewallRule);
+        }
+    }
+}
