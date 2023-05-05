08f020b1255a4a20b55345a7e518075bf48a8d84
DG Hades <61195031+DGHades@users.noreply.github.com>
2023-05-06 01:42:19 +0200

    Add base for c# rule delete

--- ToggleRule/ToggleRule/Program.cs
+++ ToggleRule/ToggleRule/Program.cs
@@ -4,7 +4,13 @@ namespace ToggleRule
 {
     internal class Program
     {
+        private static string RuleName = "LoL_Invisible";
         static void Main(string[] args)
+        {
+            
+        }
+
+        static void CreateRule()
         {
             INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
             firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
@@ -12,10 +18,37 @@ namespace ToggleRule
             firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
             firewallRule.Enabled = true;
             firewallRule.Protocol = 6;
-            firewallRule.RemotePorts = "5223"; 
-            firewallRule.Name = "LoL_Invisible";
+            firewallRule.RemotePorts = "5223";
+            firewallRule.Name = RuleName;
             INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
             firewallPolicy.Rules.Add(firewallRule);
         }
+
+        static void FindRule()
+        {
+            try
+            {
+                Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+                INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+                var currentProfiles = fwPolicy2.CurrentProfileTypes;
+
+                List<INetFwRule> RuleList = new List<INetFwRule>();
+
+                foreach (INetFwRule rule in fwPolicy2.Rules)
+                {
+                    if (rule.Name.IndexOf(RuleName) != -1)
+                    {
+                        INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                        firewallPolicy.Rules.Remove(rule.Name);
+                        Console.WriteLine(rule.Name + " has been deleted from Firewall Policy");
+                    }
+                }
+            }
+            catch (Exception r)
+            {
+                Console.WriteLine("Error delete rule from firewall");
+            }
+        }
+    
     }
 }
